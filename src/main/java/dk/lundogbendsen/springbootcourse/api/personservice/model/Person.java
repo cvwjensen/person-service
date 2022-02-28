a0791ea8bf1d6eff1c1aec3717b55e4eef4bda04
Christian von Wendt-Jensen <cvwjensen@gmail.com>
2022-02-28 09:05:34 +0100

    Initial version

---
+++ src/main/java/dk/lundogbendsen/springbootcourse/api/personservice/model/Person.java
@@ -0,0 +1,17 @@
+package dk.lundogbendsen.springbootcourse.api.personservice.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Person {
+    private Long id;
+    private String name;
+    private Long age;
+}
