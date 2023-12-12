35c08d66dff4151f6ea811a50f2f5900fdb56473
cvwj <cvwjensen@gmail.com>
2023-12-12 05:57:22 +0100

    Remove Lombok to avoid confusion

--- src/main/java/org/example/personservice/model/Person.java
+++ src/main/java/org/example/personservice/model/Person.java
@@ -6,12 +6,33 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
+
 public class Person {
     private Long id;
     private String name;
     private Long age;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getAge() {
+        return age;
+    }
+
+    public void setAge(Long age) {
+        this.age = age;
+    }
 }
