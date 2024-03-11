00ca7d37eb9ffc8a5ddc57e50093c7e4b86f49c7
cvwj <christian@adamatics.com>
2024-03-11 13:52:22 +0100

    Cleanup imports

--- src/main/java/org/example/personservice/model/Person.java
+++ src/main/java/org/example/personservice/model/Person.java
@@ -1,12 +1,6 @@
 package org.example.personservice.model;
 
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-
 public class Person {
     private Long id;
     private String name;
